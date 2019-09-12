1ab0cec9ae714c8c2d910a057738a56e233a8a12
siddardhak <imsiddardha@gmail.com>
2019-09-12 16:16:06 +1000

    Update google.test.js

--- google.test.js
+++ google.test.js
@@ -1,12 +1,12 @@
 
 describe('Google', () => {
   beforeAll(async () => {
-    await page.goto('https://www.google.com/');
+    await page.goto('https://www.urbanlifesavers.org.au/');
     
   });
 
   it('Urban Life Savers match', async () => {
     await page.waitFor(3000);
-    await expect(page).toMatch('google');
+    await expect(page).toMatch('NO MORE DEATHS FROM CARDIAC ARREST');
   });
 });
