c9db5ca1c4af9ea3773c0e9aec00f41e545ba443
siddardhak <imsiddardha@gmail.com>
2019-09-13 10:48:11 +1000

    Update google.test.js

--- google.test.js
+++ google.test.js
@@ -1,13 +1,12 @@
 
 describe('Google', () => {
   beforeAll(async () => {
-    jest.setTimeout(10000);
     await page.goto('https://www.urbanlifesavers.org.au/');
     
   });
 
   it('Urban Life Savers testing', async () => {
-    await page.waitFor(4000);
+    await page.waitFor(5000);
     await expect(page).toMatch('NO MORE DEATHS FROM CARDIAC ARREST');
   });
 });
